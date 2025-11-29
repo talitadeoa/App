5170f18a5a179b4872c4f8e7dbfcfb381bd73565
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2025-11-29 13:05:01 -0300

    .

--- cosmos/src/Main.tsx
+++ cosmos/src/Main.tsx
@@ -1,7 +1,7 @@
 // src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import App from './App';
 import './styles.css'; // <- CSS único aqui
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
